bfcf9686b62904645ae607ef0eb32de80e1a6b45
Denis Badurina <badurinadenis@gmail.com>
2019-07-05 17:26:37 +0200

    feat(jss): minify class names on in production

--- packages/domonda-ui/src/styles/jss.ts
+++ packages/domonda-ui/src/styles/jss.ts
@@ -9,6 +9,29 @@ import propsSort from 'jss-plugin-props-sort';
 // Sets up the JSS instance by injecting necessary plugins.
 export function install() {
   return jss.setup({
+    createGenerateId:
+      process.env.NODE_ENV === 'production'
+        ? () => {
+            let ruleCounter = 0;
+            return (_0, sheet) => {
+              ruleCounter += 1;
+
+              let prefix = '';
+              let jssId = '';
+              if (sheet) {
+                if (sheet.options.classNamePrefix) {
+                  prefix = sheet.options.classNamePrefix;
+                }
+                // id can indeed exist (copied from the official JSS repo)
+                if ((sheet.options.jss as any).id != null) {
+                  jssId = String((sheet.options.jss as any).id);
+                }
+              }
+
+              return `${prefix || 'c'}${jssId}${ruleCounter}`;
+            };
+          }
+        : undefined,
     plugins: [functions(), global(), nested(), camelCase(), vendorPrefixer(), propsSort()],
   });
 }
