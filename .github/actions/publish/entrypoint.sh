0f16c493419a19cbbe6c4594c08d6341f709f866
Denis Badurina <badurinadenis@gmail.com>
2019-07-05 13:47:17 +0200

    chore: fix CI permissions for publishing

--- .github/actions/publish/entrypoint.sh
+++ .github/actions/publish/entrypoint.sh
@@ -3,7 +3,7 @@
 set -eu
 
 if [ -n "$NPM_AUTH_TOKEN" ]; then
-  echo "//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > ~/.npmrc
+  printf "unsafe-perm = true\n//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > .npmrc
 fi
 
 sh -c "npm $*"
