9102d1ce2e2b00b92fdf5c5e26a345b17fc5386b
Denis Badurina <badurinadenis@gmail.com>
2019-07-05 12:03:26 +0200

    chore(scripts): more customizable copy-files

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -1,35 +1,64 @@
 /* eslint-disable no-console */
 const path = require('path');
 const fse = require('fs-extra');
+const pick = require('lodash/pick');
 
 const packagePath = process.cwd();
-const buildPath = path.join(packagePath, './lib');
 
-async function includeFileInBuild(file) {
+async function readPackageData() {
+  const packageData = await fse.readFile(path.resolve(packagePath, './package.json'), 'utf8');
+  return JSON.parse(packageData);
+}
+
+async function getLibDir() {
+  const packageData = await readPackageData();
+  return packageData.libDir || './lib';
+}
+
+async function includeFileInLib(file) {
   const sourcePath = path.resolve(packagePath, file);
-  const targetPath = path.resolve(buildPath, path.basename(file));
+  const libDir = await getLibDir();
+  const targetPath = path.resolve(libDir, path.basename(file));
+
   await fse.copy(sourcePath, targetPath);
+
   console.log(`Copied ${sourcePath} to ${targetPath}`);
 }
 
-async function createPackageFile() {
-  const packageData = await fse.readFile(path.resolve(packagePath, './package.json'), 'utf8');
-  const { scripts, devDependencies, ...packageDataOther } = JSON.parse(packageData);
-  const newPackageData = {
-    ...packageDataOther,
+async function createLibPackage() {
+  const includedLibPackageDataKeys = [
+    'name',
+    'version',
+    'description',
+    'keywords',
+    'homepage',
+    'license',
+    'repository',
+    'publishConfig',
+    'bugs',
+    'peerDependencies',
+    'dependencies',
+  ];
+
+  const packageData = await readPackageData();
+  const libPackageData = {
+    ...pick(packageData, includedLibPackageDataKeys),
     main: './index.js',
     typings: './index.d.ts',
   };
-  const targetPath = path.resolve(buildPath, './package.json');
 
-  await fse.writeFile(targetPath, JSON.stringify(newPackageData, null, 2), 'utf8');
-  console.log(`Created package.json in ${targetPath}`);
+  const libDir = await getLibDir();
+  const targetPath = path.resolve(libDir, './package.json');
+
+  await fse.writeFile(targetPath, JSON.stringify(libPackageData, null, 2), 'utf8');
+
+  console.log(`Created library package.json in ${targetPath}`);
 }
 
 async function run() {
   try {
-    await createPackageFile();
-    await Promise.all(['./CHANGELOG.md', './LICENSE'].map((file) => includeFileInBuild(file)));
+    await createLibPackage();
+    await Promise.all(['./CHANGELOG.md', './LICENSE'].map((file) => includeFileInLib(file)));
   } catch (err) {
     console.error(err);
     process.exit(1);
