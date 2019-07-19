2dcbf93d4acd557fdb01b3b2a08943dd77247c09
Denis Badurina <badurinadenis@gmail.com>
2019-07-19 12:31:44 +0200

    fix(storybook): use same react instance

--- packages/domonda-react-form/.storybook/webpack.config.js
+++ packages/domonda-react-form/.storybook/webpack.config.js
@@ -37,6 +37,8 @@ module.exports = ({ config }) => {
 
   config.resolve.extensions.push('.ts', '.tsx');
   config.resolve.alias = {
+    react: path.resolve(__dirname, '../node_modules/react'),
+    'react-dom': path.resolve(__dirname, '../node_modules/react-dom'),
     '@domonda/form': path.resolve(__dirname, '../../domonda-form/src'),
     '@domonda/ui': path.resolve(__dirname, '../../domonda-ui/src'),
   };
