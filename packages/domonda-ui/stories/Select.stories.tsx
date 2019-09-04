b34ccc384c75e7a191fe28d034056e4e5ee0631b
Denis Badurina <badurinadenis@gmail.com>
2019-09-04 12:57:19 +0200

    fix(Select.stories): proper dom nesting and drop `selected` prop

--- packages/domonda-ui/stories/Select.stories.tsx
+++ packages/domonda-ui/stories/Select.stories.tsx
@@ -6,58 +6,58 @@ const stories = storiesOf('Select', module);
 
 stories.add('Overview', () => (
   <>
-    <p>
+    <div style={{ margin: '1em 0' }}>
       <Select>
-        <option disabled selected value="">
+        <option disabled value="">
           No label
         </option>
         <option value={10}>Ten</option>
         <option value={20}>Twenty</option>
         <option value={30}>Thirty</option>
       </Select>
-    </p>
-    <p>
+    </div>
+    <div style={{ margin: '1em 0' }}>
       <Select dense>
-        <option disabled selected value="">
+        <option disabled value="">
           Dense
         </option>
         <option value={10}>Ten</option>
         <option value={20}>Twenty</option>
         <option value={30}>Thirty</option>
       </Select>
-    </p>
-    <p>
+    </div>
+    <div style={{ margin: '1em 0' }}>
       <Select dense label="Dense with label">
-        <option disabled selected value="">
+        <option disabled value="">
           &mdash;
         </option>
         <option value={10}>Ten</option>
         <option value={20}>Twenty</option>
         <option value={30}>Thirty</option>
       </Select>
-    </p>
-    <p>
+    </div>
+    <div style={{ margin: '1em 0' }}>
       <Select label="Invoice type">
         <option value="incoming">Incoming</option>
         <option value="outgoing">Outgoing</option>
         <option value="credit-note">Credit-note</option>
       </Select>
-    </p>
-    <p>
+    </div>
+    <div style={{ margin: '1em 0' }}>
       <Select label="Country" required>
-        <option disabled selected value="">
+        <option disabled value="">
           &mdash;
         </option>
         <option value="at">Austria</option>
         <option value="ba">Bosnia and Herzegovina</option>
       </Select>
-    </p>
-    <p>
+    </div>
+    <div style={{ margin: '1em 0' }}>
       <Select label="Disabled" disabled>
-        <option disabled selected value="">
+        <option disabled value="">
           &mdash;
         </option>
       </Select>
-    </p>
+    </div>
   </>
 ));
