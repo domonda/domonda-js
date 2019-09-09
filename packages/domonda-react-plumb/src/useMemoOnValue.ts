10050c101cbfdb8ce9a8f0c9016a1a96c9f639af
Denis Badurina <badurinadenis@gmail.com>
2019-09-09 11:28:04 +0200

    feat(useValueIsEqual): introduce

--- packages/domonda-react-plumb/src/useMemoOnValue.ts
+++ packages/domonda-react-plumb/src/useMemoOnValue.ts
@@ -7,20 +7,21 @@
 import { useRef } from 'react';
 import { deepEqual, shallowEqual } from 'fast-equals';
 
-// Memoizes the passed value by performing a deep compare.
-export function useDeepMemoOnValue<T>(value: T): T {
+// Memoizes the passed value by performing the compairson defined in `valueIsEqual`;
+export function useValueIsEqual<T>(value: T, valueIsEqual: (prev: T, next: T) => boolean): T {
   const ref = useRef<T>(value);
-  if (!deepEqual(ref.current, value)) {
+  if (!valueIsEqual(ref.current, value)) {
     ref.current = value;
   }
   return ref.current;
 }
 
+// Memoizes the passed value by performing a deep compare.
+export function useDeepMemoOnValue<T>(value: T): T {
+  return useValueIsEqual(value, deepEqual);
+}
+
 // Memoizes the passed value by performing a shallow compare.
 export function useShallowMemoOnValue<T>(value: T): T {
-  const ref = useRef<T>(value);
-  if (!shallowEqual(ref.current, value)) {
-    ref.current = value;
-  }
-  return ref.current;
+  return useValueIsEqual(value, shallowEqual);
 }
