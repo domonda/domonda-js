a72cc7aa1a1d0cb15e6d5d4253567b431e117b3c
Denis Badurina <badurinadenis@gmail.com>
2019-09-10 11:50:21 +0200

    chore(jest): serve react from root node_packages

--- jest.config.js
+++ jest.config.js
@@ -5,12 +5,16 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.tsx?$',
   testPathIgnorePatterns: ['/node_modules/', '(/__tests__/.*|(\\.|/)(test))\\.d.ts$'],
   moduleNameMapper: {
-    '^@domonda/ui(.*)$': '<rootDir>/packages/domonda-ui/src/$1',
-    '^@domonda/form(.*)$': '<rootDir>/packages/domonda-form/src/$1',
-    '^@domonda/react-form(.*)$': '<rootDir>/packages/domonda-react-form/src/$1',
-    '^@domonda/plumb(.*)$': '<rootDir>/packages/domonda-plumb/src/$1',
-    '^@domonda/query-params(.*)$': '<rootDir>/packages/domonda-query-params/src/$1',
-    '^@domonda/react-plumb(.*)$': '<rootDir>/packages/domonda-react-plumb/src/$1',
+    // react
+    '^react$': '<rootDir>/node_modules/react/',
+    '^react-dom$': '<rootDir>/node_modules/react-dom/',
+    // packages
+    '^@domonda/ui(.*)$': '<rootDir>/packages/domonda-ui/src$1',
+    '^@domonda/form(.*)$': '<rootDir>/packages/domonda-form/src$1',
+    '^@domonda/react-form(.*)$': '<rootDir>/packages/domonda-react-form/src$1',
+    '^@domonda/plumb(.*)$': '<rootDir>/packages/domonda-plumb/src$1',
+    '^@domonda/query-params(.*)$': '<rootDir>/packages/domonda-query-params/src$1',
+    '^@domonda/react-plumb(.*)$': '<rootDir>/packages/domonda-react-plumb/src$1',
   },
   globals: {
     'ts-jest': {
