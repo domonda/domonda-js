0a2f9da0df7e7ed4c480ddbb91d37ee05f92dfc8
Denis Badurina <badurinadenis@gmail.com>
2019-09-10 12:34:52 +0200

    fix(FormState.test): drop `only`

--- packages/domonda-react-form/__tests__/FormState.test.tsx
+++ packages/domonda-react-form/__tests__/FormState.test.tsx
@@ -212,7 +212,7 @@ describe('Updating', () => {
     expect(spy).toBeCalledTimes(1);
   });
 
-  it('should correctly handle changed status when default values change', () => {
+  it.only('should correctly handle changed status when default values change', () => {
     const spy = jest.fn((_0) => null);
 
     const dv = {
