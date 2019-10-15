efcc3396eedd5896cb78b328f6416102f3ee0cfb
Goran Simic <goransimic92@gmail.com>
2019-10-15 13:05:39 +0200

    feat(Menu): introduce possibility to toggle menu

--- packages/domonda-ui/stories/Menu.stories.tsx
+++ packages/domonda-ui/stories/Menu.stories.tsx
@@ -2,13 +2,28 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 
 import { Menu, MenuItem } from '../src/Menu';
+import { Button } from '../src/Button';
 
 const stories = storiesOf('Menu', module);
 
-stories.add('Overview', () => (
-  <Menu>
-    <MenuItem onClick={() => {}}>News</MenuItem>
-    <MenuItem onClick={() => {}}>About us</MenuItem>
-    <MenuItem onClick={() => {}}>Contact</MenuItem>
-  </Menu>
-));
+const SimpleMenu = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) =>
+    setAnchorEl(event.currentTarget);
+
+  const handleClose = () => setAnchorEl(null);
+
+  return (
+    <div>
+      <Button onClick={handleClick}>Open Menu</Button>
+      <Menu anchorEl={anchorEl} open={!!anchorEl} onClose={handleClose}>
+        <MenuItem onClick={handleClose}>News</MenuItem>
+        <MenuItem onClick={handleClose}>About us</MenuItem>
+        <MenuItem onClick={handleClose}>Contact</MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+stories.add('Overview', () => <SimpleMenu />);
