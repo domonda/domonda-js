e134bee59630bdb2a2b194170dd5b1e4c28aa868
Denis Badurina <badurinadenis@gmail.com>
2019-10-31 15:44:46 +0100

    feat(SvgIcon): removed
    BREAKING CHANGE: SvgIcon is dropped in favor of native svg support.

--- packages/domonda-ui/src/index.ts
+++ packages/domonda-ui/src/index.ts
@@ -2,7 +2,6 @@ export * from './styles';
 export * from './Baseline';
 export * from './Button';
 export * from './Menu';
-export * from './SvgIcon';
 export * from './Input';
 export * from './Select';
 export * from './Err';
