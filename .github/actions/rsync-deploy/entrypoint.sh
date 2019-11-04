76e6531d3286fcd0540be039cad20781b7985078
Denis Badurina <badurinadenis@gmail.com>
2019-11-04 16:48:43 +0100

    chore(rsync): simplify command and storybook publish fix

--- .github/actions/rsync-deploy/entrypoint.sh
+++ .github/actions/rsync-deploy/entrypoint.sh
@@ -7,4 +7,4 @@ mkdir "$SSH_PATH"
 echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
-sh -c "rsync $1 -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $2 $GITHUB_WORKSPACE/ $3"
+sh -c "rsync $1 -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$2 $3"
