cc74eff19a6ffee69a5e4f11c43bc169243b4fb3
Denis Badurina <badurinadenis@gmail.com>
2019-11-04 16:20:58 +0100

    chore(storybook): publish domonda-ui storybook to ui.domonda.dev

---
+++ .github/actions/rsync-deploy/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -eu
+
+SSH_PATH="$HOME/.ssh"
+mkdir "$SSH_PATH"
+echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
+chmod 600 "$SSH_PATH/deploy_key"
+
+sh -c "rsync $1 -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $2 $GITHUB_WORKSPACE/ $3"
