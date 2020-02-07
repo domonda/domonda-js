983075d7a4ead4cf8fd05acf96477676b6d7bdac
Goran Simic <goransimic92@gmail.com>
2020-02-07 15:39:40 +0100

    refactor(Accordion): use treat

---
+++ packages/domonda-ui/src/Accordion/Accordion.treat.ts
@@ -0,0 +1,59 @@
+import { styleMap, globalStyle } from '../styles/treat';
+
+export const styles = styleMap(({ spacing }) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  button: {
+    // reset
+    border: 0,
+    margin: 0,
+    width: 'auto',
+    overflow: 'visible',
+    textAlign: 'inherit',
+    background: 'transparent',
+    lineHeight: '1em',
+    webkitFontSmoothing: 'inherit',
+    mozOsxFontSmoothing: 'inherit',
+    webkitAppearance: 'none',
+    outline: 'none',
+    textDecoration: 'none',
+    fontSize: 'inherit',
+    // ./reset
+    display: 'flex',
+    alignItems: 'center',
+    padding: spacing('small'),
+    cursor: 'pointer',
+  },
+  labelContent: {
+    flex: 1,
+  },
+  labelIcon: {
+    marginLeft: spacing('tiny'),
+  },
+  content: {
+    padding: spacing('tiny', 'small', 'small', 'small'),
+  },
+  open: {},
+}));
+
+// open
+globalStyle(`${styles.root}${styles.open}`, ({ palette }) => ({
+  borderBottom: `1px solid ${palette.gray08}`,
+}));
+
+// hover
+globalStyle(`${styles.button}:hover`, ({ palette }) => ({
+  backgroundColor: palette.lightest('gray08'),
+}));
+
+// focus
+globalStyle(`${styles.button}:focus`, ({ palette }) => ({
+  outline: `2px solid ${palette.light('primary')}`,
+}));
+
+// active
+globalStyle(`${styles.button}:active`, () => ({
+  outline: 'none',
+}));
