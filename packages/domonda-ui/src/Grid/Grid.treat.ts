91a3c33fea6bc586351bb25db56046a50493e0b5
Goran Simic <goransimic92@gmail.com>
2020-02-11 08:54:04 +0100

    refactor(Grid): use treat

---
+++ packages/domonda-ui/src/Grid/Grid.treat.ts
@@ -0,0 +1,18 @@
+import { styleMap } from '../styles/treat';
+
+export const styles = styleMap(() => ({
+  container: {
+    display: 'grid',
+    width: '100%',
+  },
+  fill: {
+    width: '100%',
+    height: '100%',
+  },
+  overflowing: {
+    overflow: 'auto',
+    backfaceVisibility: 'hidden',
+    willChange: 'scroll-position',
+    WebkitOverflowScrolling: 'touch',
+  },
+}));
