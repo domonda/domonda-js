66d6dd6dee46e2b0684619b73ae76b2d1525bec9
Denis Badurina <badurinadenis@gmail.com>
2020-02-25 13:20:21 +0100

    style: drop code comments & put explenation in head comment

--- packages/domonda-react-form/src/FormRadioField/useFormRadioField.ts
+++ packages/domonda-react-form/src/FormRadioField/useFormRadioField.ts
@@ -2,18 +2,17 @@
  *
  * FormRadioField/useFormRadioField
  *
+ * In which input to display the validity message is undecidable given more radio inputs.
+ * Therefore we don't have a `ref` pointing to the child radio inputs.
+ *
  */
 
-import React, {
-  useCallback,
-  // useState,
-  // useEffect
-} from 'react';
+import React, { useCallback } from 'react';
 import { useFormField, UseFormFieldProps, FormFieldAPI, FormFieldValidate } from '../FormField';
 
 export type FormRadioFieldValidate = FormFieldValidate<string | null>;
 
-export interface UseFormRadioFieldProps extends UseFormFieldProps<string | null> {
+export interface UseFormRadioFieldProps extends Omit<UseFormFieldProps<string | null>, 'validate'> {
   required?: boolean;
 }
 
@@ -24,46 +23,16 @@ export interface FormRadioFieldAPI extends FormFieldAPI<string | null> {
     required: boolean | undefined;
     disabled: boolean;
     readOnly: boolean;
-    // ref: (element: HTMLInputElement | null) => void;
     onChange: React.ChangeEventHandler<HTMLInputElement>;
   };
 }
 
 export function useFormRadioField(props: UseFormRadioFieldProps): FormRadioFieldAPI {
-  const { required, validate, ...formFieldProps } = props;
+  const { required, ...formFieldProps } = props;
   const formField = useFormField<string | null>(formFieldProps);
 
-  if (validate) {
-    throw new Error('`validate` prop is not supported on the `FormRadioField` component!');
-  }
-
-  /**
-   * In which input to display the validity message is undecidable given more radio inputs
-   */
-
-  // const [inputEl, setInputEl] = useState<HTMLInputElement | null>(null);
-
-  // const { validityMessage } = formField.state;
-  // useEffect(() => {
-  //   if (inputEl) {
-  //     inputEl.setCustomValidity(validityMessage || '');
-  //     if (validityMessage) {
-  //       inputEl.reportValidity();
-  //     }
-  //   }
-  // }, [validityMessage || '']);
-
   const handleChange = useCallback<React.FormEventHandler<HTMLInputElement>>(
-    ({ currentTarget }) => {
-      formField.setValue(currentTarget.value || null);
-
-      // we want this asynchronously called because some browsers (safari) decide to hide the validity box on input
-      const { validity } = currentTarget;
-      if (!validity.valid) {
-        const reportValidity = () => currentTarget.reportValidity();
-        setTimeout(reportValidity, 0);
-      }
-    },
+    ({ currentTarget }) => formField.setValue(currentTarget.value || null),
     [formField.setValue],
   );
 
@@ -75,7 +44,6 @@ export function useFormRadioField(props: UseFormRadioFieldProps): FormRadioField
       required,
       disabled: formField.state.disabled,
       readOnly: formField.state.readOnly,
-      // ref: setInputEl,
       onChange: handleChange,
     },
   };
