fcb4ec7fc2a6dfc4aacdefb8d809f61d90f913c0
Goran Simic <goransimic92@gmail.com>
2020-03-06 10:45:32 +0100

    test(FormArrayField): add more tests

--- packages/domonda-react-form/__tests__/FormArrayField.test.tsx
+++ packages/domonda-react-form/__tests__/FormArrayField.test.tsx
@@ -3,11 +3,14 @@
  */
 
 import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import { renderHook, act } from '@testing-library/react-hooks';
-import { createForm } from '@domonda/form';
+import { createForm, Form as DomondaForm } from '@domonda/form';
 
+import { Form } from '../src/Form';
 import { FormContext } from '../src/FormContext';
-import { useFormArrayField, UseFormArrayFieldProps } from '../src/FormArrayField';
+import { FormField } from '../src/FormField';
+import { useFormArrayField, UseFormArrayFieldProps, FormArrayField } from '../src/FormArrayField';
 
 describe('Creation', () => {
   it('should set field value to null without allowEmptyArray prop', () => {
@@ -122,5 +125,43 @@ describe('Update', () => {
     });
 
     expect(form.values[path]).toEqual([]);
+  });
+
+  it('should remove path to the value from the values', (done) => {
+    interface DefaultValues {
+      people: string[];
+    }
+
+    const defaultValues = { people: ['John', 'Jane'] };
+
+    let form: DomondaForm<DefaultValues>;
+
+    const { getByText } = render(
+      <Form getForm={(f) => (form = f)} defaultValues={defaultValues}>
+        <FormArrayField path="people">
+          {({ items, remove }) => (
+            <>
+              {items &&
+                items.map((_0, index) => (
+                  <FormField key={index.toString()} path={`people[${index}]`}>
+                    {() => null}
+                  </FormField>
+                ))}
+
+              <button type="button" onClick={remove}>
+                -
+              </button>
+            </>
+          )}
+        </FormArrayField>
+      </Form>,
+    );
+
+    fireEvent.click(getByText('-'));
+
+    setTimeout(() => {
+      expect(form.state.values.people).toEqual(['John']);
+      done();
+    }, 0);
   });
 });
