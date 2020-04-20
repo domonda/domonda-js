8c4dfd685f020dde9cf5a99c6730d0263c7d1f54
Denis Badurina <badurinadenis@gmail.com>
2020-04-20 11:27:28 +0200

    ci(rsync): use action from separate repo

--- .github/actions/publish/entrypoint.sh
+++ .github/actions/publish/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-
 set -eu
 
 if [ -n "$NPM_AUTH_TOKEN" ]; then
-  printf "unsafe-perm = true\n//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN" > .npmrc
+  printf "unsafe-perm = true\n//registry.npmjs.org/:_authToken=%s" "$NPM_AUTH_TOKEN" > .npmrc
 fi
 
 sh -c "npm $*"
