9a420c28f47547cea41d7aaa7106f70becdd179c
Denis Badurina <badurinadenis@gmail.com>
2020-05-13 17:02:35 +0200

    build(jest): fix moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.tsx?$',
   testPathIgnorePatterns: ['/node_modules/', '(/__tests__/.*|(\\.|/)(test))\\.d.ts$'],
   moduleNameMapper: {
+    '^@domonda/(.*)/(.*)$': '<rootDir>/packages/$1/src/$2',
     '^@domonda/(.*)$': '<rootDir>/packages/$1/src',
-    '^@domonda/(.*)$/(.*)$': '<rootDir>/packages/$1/src/$2',
   },
 };
