bd1908901bd4cdf0f91dc0094092f1b0de8d63ee
Denis Badurina <badurinadenis@gmail.com>
2020-05-14 15:15:06 +0200

    fix(react-form): use babel for storybook transpilation

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   presets: [['@babel/env', { targets: { node: 'current' } }], '@babel/typescript', '@babel/react'],
+  plugins: [
+    '@babel/proposal-class-properties',
+    '@babel/proposal-object-rest-spread',
+    '@babel/proposal-nullish-coalescing-operator',
+    '@babel/proposal-optional-chaining',
+  ],
 };
--- packages/react-form/.storybook/webpack.config.js
+++ packages/react-form/.storybook/webpack.config.js
@@ -1,49 +1,29 @@
 const path = require('path');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
-const tsconfigPath = path.resolve(__dirname, 'tsconfig.json');
 
 module.exports = ({ config }) => {
-  const dev = config.mode === 'development';
-
   config.resolve.extensions.push('.ts', '.tsx');
+
   config.resolve.alias = {
-    react: path.resolve(__dirname, '../node_modules/react'),
-    'react-dom': path.resolve(__dirname, '../node_modules/react-dom'),
     '@domonda/form': path.resolve(__dirname, '../../form/src'),
     '@domonda/ui': path.resolve(__dirname, '../../ui/src'),
+    '@domonda/plumb': path.resolve(__dirname, '../../plumb/src'),
+    '@domonda/react-plumb': path.resolve(__dirname, '../../react-plumb/src'),
   };
 
   config.module.rules.push({
-    test: /\.(ts|tsx)$/,
+    test: /\.tsx?$/,
+    exclude: [/node_modules/, /__tests__/],
     use: [
       {
-        loader: require.resolve('ts-loader'),
+        loader: 'babel-loader',
         options: {
-          transpileOnly: !dev,
-          configFile: tsconfigPath,
+          root: path.resolve(__dirname, '../../..'),
+          cacheDirectory: true,
+          cacheCompression: false, // unnecessary compression
         },
-      },
-      {
-        loader: require.resolve('react-docgen-typescript-loader'),
       },
     ],
   });
-
-  // https://github.com/TypeStrong/ts-loader/tree/87340f49e2ebfd5158c26dbcfef1b2beb177f7c3#transpileonly
-  config.stats = {
-    ...config.stats,
-    warningsFilter: /export .* was not found in/,
-  };
-
-  if (dev) {
-    config.plugins.push(
-      new ForkTsCheckerWebpackPlugin({
-        tsconfig: tsconfigPath,
-        checkSyntacticErrors: true,
-      }),
-    );
-  }
 
   return config;
 };
