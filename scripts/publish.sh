c3f6a0d6d9620f1bf838444beaba4990771e60a3
Denis Badurina <badurinadenis@gmail.com>
2020-05-15 11:20:25 +0200

    ci: automated publish WIP

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,10 +2,9 @@
 set -ex
 
 REPO="domonda/domonda-js"
-GITHUB_USER=${GITHUB_USER:-domondabot}
 GITHUB_PAT=${GITHUB_PAT:-"<undefined>"}
 
-curl -X POST -u "$GITHUB_USER:$GITHUB_PAT" \
+curl -X POST -u "github:$GITHUB_PAT" \
   -H "Accept: application/vnd.github.everest-preview+json"  \
   -H "Content-Type: application/json" \
   --data '{ "event_type": "publish" }' \
