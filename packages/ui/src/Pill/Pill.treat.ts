9cc5307175f92ac5ce43432abfde486d55f33522
enisdenjo <badurinadenis@gmail.com>
2020-07-03 18:52:51 +0200

    feat(Pill): refresh

--- packages/ui/src/Pill/Pill.treat.ts
+++ packages/ui/src/Pill/Pill.treat.ts
@@ -1,14 +1,15 @@
-import { style, styleMap, Style } from 'treat';
-import { COLORS } from '../styles/palette';
+import { style, styleMap, Style, globalStyle } from 'treat';
+import { COLORS, Color } from '../styles/palette';
+import { root as typographyRoot } from '../Typography/Typography.treat';
 
 export const root = style(({ shape, sizing }) => ({
   display: 'inline-flex',
-  padding: `${sizing('tiny') / 2}px ${sizing('tiny')}px`,
+  padding: `calc(${sizing('small')} / 2) ${sizing('small')}`,
   borderRadius: shape.borderRadius.pill,
 }));
 
 export const colors = styleMap(({ palette }) => ({
-  ...COLORS.reduce<Record<string, Style>>(
+  ...COLORS.reduce(
     (acc, color) => ({
       ...acc,
       [color]: {
@@ -16,6 +17,12 @@ export const colors = styleMap(({ palette }) => ({
         color: palette[color],
       },
     }),
-    {},
+    {} as Record<Color, Style>,
   ),
 }));
+
+Object.entries(colors).forEach(([color, colorClass]) => {
+  globalStyle(`${root}${colorClass} ${typographyRoot}`, ({ palette }) => ({
+    color: palette[color as Color],
+  }));
+});
