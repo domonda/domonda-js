63877e4a9ed31d4b689612e049a38b90a00f5e41
Denis Badurina <denis@domonda.com>
2020-07-14 17:25:01 +0200

    refactor: resolve merge conflict

--- packages/ui/src/Dialog/Dialog.tsx
+++ packages/ui/src/Dialog/Dialog.tsx
@@ -6,18 +6,10 @@
 
 import React from 'react';
 import clsx from 'clsx';
-import { TransitionProps } from 'react-transition-group/Transition';
-
-import { useStyles } from 'react-treat';
-import { defaultDuration, TransitionHandlerProps } from '../styles/transition';
 import { capitalize } from '../utils';
-
+import { useStyles } from 'react-treat';
 import { Backdrop } from '../Backdrop';
-<<<<<<< HEAD
-=======
-import { Fade } from '../Fade';
 import { Modal, ModalProps } from '../Modal';
->>>>>>> refactor(Dialog): use treat
 import { Paper, PaperProps } from '../Paper';
 
 import * as styles from './Dialog.treat';
@@ -37,108 +29,31 @@ export interface DialogProps extends ExtendingProps {
 /**
  * Dialogs are overlaid modal paper based components with a backdrop.
  */
-<<<<<<< HEAD
-const Dialog = React.forwardRef<HTMLDivElement, DialogProps & WithStyles<typeof styles>>(
-  function Dialog(props, ref) {
-    const {
-      BackdropProps,
-      children,
-      classes,
-      className,
-      disableBackdropClick = false,
-      disableEscapeKeyDown = false,
-      fullScreen = false,
-      fullWidth = false,
-      maxWidth = 'sm',
-      onBackdropClick,
-      onClose,
-      onEscapeKeyDown,
-      open,
-      PaperComponent = Paper,
-      PaperProps = {},
-      scroll = 'paper',
-      ...other
-    } = props;
-
-    const mouseDownTarget = React.useRef<any>();
-    const handleMouseDown = (event: React.MouseEvent) => {
-      mouseDownTarget.current = event.target;
-    };
-    const handleBackdropClick = (event: React.MouseEvent) => {
-      // Ignore the events not coming from the "backdrop"
-      // We don't want to close the dialog when clicking the dialog content.
-      if (event.target !== event.currentTarget) {
-        return;
-      }
-
-      // Make sure the event starts and ends on the same DOM element.
-      if (event.target !== mouseDownTarget.current) {
-        return;
-      }
-
-      mouseDownTarget.current = null;
-
-      if (onBackdropClick) {
-        onBackdropClick(event);
-      }
-
-      if (!disableBackdropClick && onClose) {
-        onClose(event, 'backdropClick');
-      }
-    };
-
-    return (
-      <Modal
-        className={clsx(classes.root, className)}
-        BackdropComponent={Backdrop}
-        BackdropProps={BackdropProps}
-        closeAfterTransition
-        disableBackdropClick={disableBackdropClick}
-        disableEscapeKeyDown={disableEscapeKeyDown}
-        onEscapeKeyDown={onEscapeKeyDown}
-        onClose={onClose}
-        open={open}
-        ref={ref}
-        {...other}
-      >
-=======
 export const Dialog = React.forwardRef<HTMLDivElement, DialogProps>(function Dialog(props, ref) {
   const {
+    BackdropProps,
     children,
     className,
-    BackdropProps,
     disableBackdropClick = false,
     disableEscapeKeyDown = false,
     fullScreen = false,
     fullWidth = false,
     maxWidth = 'sm',
+    onBackdropClick,
+    onClose,
+    onEscapeKeyDown,
     open,
     PaperComponent = Paper,
     PaperProps = {},
     scroll = 'paper',
-    TransitionComponent = Fade,
-    transitionDuration = defaultTransitionDuration,
-    TransitionProps,
-    onBackdropClick,
-    onClose,
-    onEnter,
-    onEntered,
-    onEntering,
-    onEscapeKeyDown,
-    onExit,
-    onExited,
-    onExiting,
     ...other
   } = props;
-
   const classes = useStyles(styles);
 
   const mouseDownTarget = React.useRef<any>();
-
   const handleMouseDown = (event: React.MouseEvent) => {
     mouseDownTarget.current = event.target;
   };
-
   const handleBackdropClick = (event: React.MouseEvent) => {
     // Ignore the events not coming from the "backdrop"
     // We don't want to close the dialog when clicking the dialog content.
@@ -164,83 +79,44 @@ export const Dialog = React.forwardRef<HTMLDivElement, DialogProps>(function Dia
 
   return (
     <Modal
-      ref={ref}
-      BackdropComponent={Backdrop}
-      BackdropProps={{
-        transitionDuration,
-        ...BackdropProps,
-      }}
       className={clsx(classes.root, className)}
+      BackdropComponent={Backdrop}
+      BackdropProps={BackdropProps}
       closeAfterTransition
       disableBackdropClick={disableBackdropClick}
       disableEscapeKeyDown={disableEscapeKeyDown}
-      open={open}
-      onClose={onClose}
       onEscapeKeyDown={onEscapeKeyDown}
+      onClose={onClose}
+      open={open}
+      ref={ref}
       {...other}
     >
-      <TransitionComponent
-        appear
-        in={open}
-        role="none presentation"
-        timeout={transitionDuration}
-        onEnter={onEnter}
-        onEntered={onEntered}
-        onEntering={onEntering}
-        onExit={onExit}
-        onExited={onExited}
-        onExiting={onExiting}
-        {...TransitionProps}
+      <div
+        className={clsx(
+          classes.container,
+          classes[`scroll${capitalize(scroll)}` as keyof typeof classes],
+        )}
+        onClick={handleBackdropClick}
+        onMouseDown={handleMouseDown}
       >
-        {/* roles are applied via cloneElement from TransitionComponent */}
-        {/* roles needs to be applied on the immediate child of Modal or it'll inject one */}
->>>>>>> refactor(Dialog): use treat
-        <div
+        <PaperComponent
+          shadow="large"
+          role="dialog"
+          {...PaperProps}
           className={clsx(
-            classes.container,
-            classes[`scroll${capitalize(scroll)}` as keyof typeof classes],
+            classes.paper,
+            classes[`paperScroll${capitalize(scroll)}` as keyof typeof classes],
+            classes[`paperWidth${capitalize(String(maxWidth))}` as keyof typeof classes],
+            {
+              [classes.paperFullScreen]: fullScreen,
+              [classes.paperFullWidth]: fullWidth,
+            },
+            PaperProps.className,
           )}
-          onClick={handleBackdropClick}
-          onMouseDown={handleMouseDown}
         >
-          <PaperComponent
-<<<<<<< HEAD
-            shadow="large"
-            role="dialog"
-=======
-            role="dialog"
-            shadow="large"
->>>>>>> refactor(Dialog): use treat
-            {...PaperProps}
-            className={clsx(
-              classes.paper,
-              classes[`paperScroll${capitalize(scroll)}` as keyof typeof classes],
-              classes[`paperWidth${capitalize(String(maxWidth))}` as keyof typeof classes],
-              {
-                [classes.paperFullScreen]: fullScreen,
-                [classes.paperFullWidth]: fullWidth,
-              },
-              PaperProps.className,
-            )}
-          >
-            {children}
-          </PaperComponent>
-        </div>
-<<<<<<< HEAD
-      </Modal>
-    );
-  },
-);
-
-if (process.env.NODE_ENV !== 'production') {
-  Dialog.displayName = 'Dialog';
-}
-
-const StyledDialog = withStyles(styles)(Dialog);
-export { StyledDialog as Dialog };
-=======
-      </TransitionComponent>
+          {children}
+        </PaperComponent>
+      </div>
     </Modal>
   );
 });
->>>>>>> refactor(Dialog): use treat
